2c4a60495174d87c1d8f8091535b2aea563beb35
Kirill <kirill040605@gmail.com>
2025-02-26 22:07:17 -0800

    start implementation of menu

--- main.go
+++ main.go
@@ -14,10 +14,44 @@ func main() {
 	// lavcLibrary.RemoveBook()
 	// lavcLibrary.PrintAllBooks()
 	fmt.Println("Welcome to LAVC library")
+	chooseAction(&lavcLibrary)
+}
+
+func chooseAction(library *library.Library) {
+	var choice int
+
+	for {
+		printMenu()
+		fmt.Scan(&choice)
+		if choice < 1 && choice > 6 {
+			fmt.Println("Invalid choice, try again")
+			continue
+		}
+		if choice == 6 {
+			fmt.Println("Exit")
+			return
+		}
+
+		switch choice {
+
+		case 1:
+			library.PrintAllBooks()
+		case 2:
+			library.AddNewBook()
+		case 3:
+			library.FindBookByName()
+		case 4:
+			library.RemoveBook()
+		}
+	}
+
 }
 
 func printMenu() {
 	fmt.Println("1. Show all books")
-	fmt.Println("2. Add new Book")
+	fmt.Println("2. Add a new Book")
 	fmt.Println("3. Find Book by Name: ")
+	fmt.Println("4. Remove the book")
+	fmt.Println("5. Edit title of a book")
+	fmt.Println("6. Exit")
 }
